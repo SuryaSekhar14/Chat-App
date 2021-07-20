bad1c2817482609c63dc1711ea9a1a907afaed77
SuryaSekhar14 <suryasekhardatta22@gmail.com>
2021-07-20 19:50:43 +0530

    Initiated class Client

--- client.py
+++ client.py
@@ -7,3 +7,13 @@ from tkinter import simpledialog
 HOST = '127.0.0.1'
 PORT = 9090
 
+class Client:
+
+    def __init__(self, host, port):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((host, port))
+
+        msg = tkinter.Tk()
+        msg.withdraw()
+        
+
