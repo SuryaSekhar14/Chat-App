4998cf1a42f8e981952987bbb366ba9ff616eecc
SuryaSekhar14 <suryasekhardatta22@gmail.com>
2021-07-20 19:46:10 +0530

    Added handle function

--- server.py
+++ server.py
@@ -19,8 +19,19 @@ def broadcast(message):
         client.send(message) 
 
 #handle connections
-def handle():
-    
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            print(f'{nicknames[client.index(client)]} says {message}')
+            broadcast(message)
+        except:
+            index=clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            nicknames.remove(nickname)
+            break
 
 #recieve new connections
 def recieve():
@@ -40,6 +51,8 @@ def recieve():
         client.send("Connected to the server".encode('utf-8'))
 
         thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
 
 
 
