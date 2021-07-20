e8e6fda59e0ea822062332b632222c7b7f87d6e0
SuryaSekhar14 <suryasekhardatta22@gmail.com>
2021-07-20 13:30:10 +0530

    Added broadcast and recieve functions

--- server.py
+++ server.py
@@ -11,15 +11,35 @@ server.bind((HOST, PORT))
 server.listen()
 
 clients = []
-nickname = []
+nicknames = []
 
 #broadcast chats
-
-
-#recieve new connections
-
+def broadcast(message):
+    for client in clients:
+        client.send(message) 
 
 #handle connections
+def handle():
+
+#recieve new connections
+def recieve():
+    while True:
+        client, address = server.accept()
+        print(f'Connected with {str(address)}')
+
+        client.send("NICK".encode('utf-8'))
+        nickname = clients.recv(1024)
+
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f"Nickname of the clienr is {nickname}")
+        broadcast(f'{nickname} connecte to the server\n'.encode('utf-8'))
+
+        client.send("Connected to the server".encode('utf-8'))
+
+        thread = threading.Thread(target=handle, args=(client,))
+
 
 
 
