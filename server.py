d31abf0b727d3b04e2b786099a0f6cd05efe4bde
SuryaSekhar14 <suryasekhardatta22@gmail.com>
2021-07-20 13:15:51 +0530

    Added server.py

--- server.py
+++ server.py
@@ -1,6 +1,26 @@
 import socket
 import threading
 
-HOST = '127.0.0.1'
+HOST = '127.0.0.1'  #replace with server ip if over internet
 PORT= 9090
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+
+server.listen()
+
+clients = []
+nickname = []
+
+#broadcast chats
+
+
+#recieve new connections
+
+
+#handle connections
+
+
+
 
